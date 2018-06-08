2e154db627fd9e54dcdaeecb5efebf8374b3cf8f
globalhqw <globalhqw@qq.com>
2018-06-08 17:13:00 +0800

    添加项目文件。

---
+++ ConsoleApp-NetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp_NetCore
+{
+    class Program
+    {
+        /// <summary>
+        /// 主程序Main
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
