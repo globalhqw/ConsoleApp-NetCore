0bd9e15a2ec12debf5b402d87f4f0c35134b98b8
globalhqw <globalhqw@qq.com>
2018-06-10 23:49:52 +0800

    初次提交修改内容

---
+++ ConsoleApp-NetCore/Course/Generic.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_NetCore.Course
+{
+
+    #region 范型介绍
+    //创建自定义泛型类型和泛型方法，以提供自己的通用解决方案，设计类型安全的高效模式。
+    public class GenericClass
+    {
+        //type parameter T in angle brackets
+        public class GenericList<T>
+        {
+            // The nested class is also generic on T.
+            private class Node
+            {
+                // T used in non-generic constructor.
+                public Node(T t)
+                {
+                    next = null;
+                    data = t;
+                }
+
+                private Node next;
+                public Node Next
+                {
+                    get { return next; }
+                    set { next = value; }
+                }
+
+                // T as private member data type.
+                private T data;
+
+                // T as return type of property.
+                public T Data
+                {
+                    get { return data; }
+                    set { data = value; }
+                }
+            }
+
+            private Node head;
+
+            // constructor
+            public GenericList()
+            {
+                head = null;
+            }
+
+            // T as method parameter type:
+            public void AddHead(T t)
+            {
+                Node n = new Node(t);
+                n.Next = head;
+                head = n;
+            }
+
+            public IEnumerator<T> GetEnumerator()
+            {
+                Node current = head;
+
+                while (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }
+        }
+
+  
+
+        public class TestGenericList
+        {
+            public void WriteMethod()
+            {
+                // int is the type argument
+                GenericList<int> list = new GenericList<int>();
+
+                for (int x = 0; x < 10; x++)
+                {
+                    list.AddHead(x);
+                }
+
+                foreach (int i in list)
+                {
+                    System.Console.Write(i + " ");
+                }
+                System.Console.WriteLine("\n Done");
+            }
+        }
+
+    }
+
+    #endregion
+
+
+
+
+
+
+
+}
--- ConsoleApp-NetCore/Program.cs
+++ ConsoleApp-NetCore/Program.cs
@@ -1,16 +1,147 @@
 ﻿using System;
+using ConsoleApp_NetCore.Course;
 
 namespace ConsoleApp_NetCore
 {
     class Program
     {
         /// <summary>
-        /// 主程序Main
+        /// 主程序Main方法
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            #region C#教程
+
+            //C# 类型系统
+            //命名空间
+            //基本类型
+            //类和对象
+            //类和结构
+            //元组
+            //解构元组和其他类型
+            //阵列
+            //接口
+            //枚举
+            //方法
+            //Lambda 表达式
+            //属性
+            //特性
+            //索引器
+            //占位符
+            #region 泛型
+
+            //范型介绍
+            GenericClass.TestGenericList tgc = new GenericClass.TestGenericList();
+            tgc.WriteMethod();
+            //类型参数的约束
+
+
+
+
+            #endregion
+
+            //迭代器
+            //委托和事件
+            //语言集成查询(LINQ)
+            //异步编程
+            //模式匹配
+            //引用语义结合值类型
+            //表达式树
+            //互操作性
+            //文件系统和注册表
+            //异常和异常处理
+            //将代码文档化
+            //XML文档注释
+            //版本控制
+
+
+            #endregion
+
+
+            #region  C#演变阶段重点
+
+            /*
+            C# 1.0 版
+
+            C# 2.0 版
+            泛型
+            分部类型
+            匿名方法
+            可以为 null 的类型
+            迭代器
+            协变和逆变
+
+            C# 3.0 版
+            自动实现的属性
+            匿名类型
+            查询表达式
+            Lambda 表达式
+            表达式树
+            扩展方法
+
+            C# 4.0 版
+            动态绑定
+            命名参数 / 可选参数
+            泛型协变和逆变
+            嵌入的互操作类型
+
+            C# 5.0 版
+            异步成员
+            调用方信息特性
+
+            C# 6.0 版
+            静态导入
+            异常筛选器
+            属性初始值设定项
+            Expression bodied 成员
+            Null 传播器
+            字符串内插
+            nameof 运算符
+            字典初始值设定项
+
+            C# 7.0 版
+            Out 变量
+            元组和析构函数
+            模式匹配
+            本地函数
+            已扩展 expression bodied 成员
+            Ref 局部变量和返回结果
+            */
+
+            #endregion
+
+
+            #region 框架编程
+
+            //程序集和全局程序集缓存
+            //使用 async 和 await 的异步编程
+            //特性
+            //调用方信息
+            //集合
+            //协变和逆变
+            //表达式树
+            //迭代器
+            //语言集成查询(LINQ)
+            //面向对象的编程
+            //反射
+            //序列化(C# )
+            //线程处理
+
+            #endregion
+
+
+            #region 示例演练
+
+
+
+            #endregion
+
+
         }
     }
+
+
 }
